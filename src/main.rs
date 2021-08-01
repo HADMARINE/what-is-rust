80cfed65c6287ae1d01d462c7ce6b4d40c0f539e
HADMARINE <contact@hadmarine.com>
2021-08-01 20:17:51 +0900

    Update tutorial code

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,63 @@ fn main() {
     //     println!("{}", guess);
     // }
     // println!("{}", guess);
-    closure(1)(10);
+    // closure(1)(10);
+    let str: String = if__fn().to_string();
+    println!("{}", str);
+    let __s = String::from("HELLO_WORLD");
+    first_ptr_in_rs_fn(__s[1..3].to_string());
+}
+
+fn first_ptr_in_rs_fn(s: String) {
+    println!("{}", s);
+}
+
+fn if__fn() -> i32 {
+    if true {
+        5
+    } else {
+        6
+    }
+}
+
+fn scope_fn() {
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+}
+
+fn array_index_fn() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+}
+
+fn tuples_fn() -> [(i32, f64, u8); 3] {
+    let tup: [(i32, f64, u8); 3] = [(1, 2.0, 3), (1, 2.0, 3), (1, 2.0, 3)];
+    tup
 }
 
 fn guess_provider() -> String {
@@ -55,4 +111,9 @@ fn spaces_fn() {
         let mut spaces = "      ";
         // spaces = spaces.len(); // error
     }
+}
+
+fn guess_fn() -> bool {
+    let guess: bool = "true".parse().expect("Not a number!");
+    guess
 }
